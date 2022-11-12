755b4e3ddd613fcd6f4cefed3c8331469c72dc07
James Sweet <james.sweet@protonmail.com>
2022-11-12 08:45:33 -0500

    feat(nixos): cleanups and implementation of nixos module

--- flake.nix
+++ flake.nix
@@ -1,16 +1,20 @@
 {
   description = "CORE (Common Open Research Emulator) is a tool for building virtual networks.";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    nix-filter.url = "github:numtide/nix-filter";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-    nix-filter,
+    gitignore,
   }:
     {
       overlays.default = final: prev: {
@@ -20,53 +24,96 @@
           prev.pythonPackagesExtensions
           ++ [
             (pself: pprev: {
-              core-emu = pself.callPackage ./package.nix {};
+              core-emu = pself.callPackage ./package.nix {inherit gitignore;};
             })
           ];
       };
+
+      nixosModules.default = {
+        pkgs,
+        lib,
+        config,
+        core-emu,
+        ...
+      }:
+        with lib; let
+          cfg = config.services.core-emu;
+        in {
+          options.services.core-emu = {
+            enable = mkEnableOption (mdDoc "Common Open Research Emulator");
+
+            package = mkOption {
+              type = types.path;
+              description = mdDoc "The CORE package to use";
+              default = pkgs.core-emu;
+              defaultText = literalExpression "pkgs.core-emu";
+            };
+          };
+
+          config = mkIf cfg.enable {
+            virtualisation.docker = {
+              enable = true;
+              daemon.settings = {
+                bridge = "none";
+                iptables = false;
+              };
+            };
+
+            environment.systemPackages = [pkgs.core-emu];
+
+            environment.etc."core/core.conf".source = "${pkgs.core-emu}/etc/core/core.conf";
+            environment.etc."core/logging.conf".source = "${pkgs.core-emu}/etc/core/logging.conf";
+
+            systemd.services.core-daemon = {
+              description = "Common Open Research Emulator Service";
+              wantedBy = ["multi-user.target"];
+              after = ["docker.service"];
+              serviceConfig = {
+                Type = "notify";
+                WatchdogSec = "10";
+                NotifyAccess = "all";
+                ExecStart = let
+                  watchdog = pkgs.writeShellScriptBin "core-daemon-watchdog" ''
+                    watchdog() {
+                      READY=0;
+
+                      PID=$1
+                      while(true); do
+                          FAIL=0
+                          $(${pkgs.netcat}/bin/nc -z -w 1 localhost 50051 &> /dev/null) || FAIL=1
+                          if [[ $FAIL -eq 0 ]]; then
+                              if [[ $READY -eq 0 ]]; then
+                                ${pkgs.systemdMinimal}/bin/systemd-notify --ready
+                              fi
+                              ${pkgs.systemdMinimal}/bin/systemd-notify WATCHDOG=1;
+                              sleep $(($WATCHDOG_USEC / 1000000 / 2))
+                          else
+                              sleep 1
+                          fi
+                      done
+                    }
+                    watchdog $$ &
+                    exec ${pkgs.core-emu}/bin/core-daemon
+                  '';
+                in "${watchdog}/bin/core-daemon-watchdog";
+              };
+            };
+          };
+        };
     }
     // flake-utils.lib.eachSystem [flake-utils.lib.system.x86_64-linux] (system: let
       pkgs = import nixpkgs {
         inherit system;
-        overlays = [
-          self.overlays.default
-          nix-filter.overlays.default
-        ];
+        overlays = [self.overlays.default];
       };
     in rec {
-      devShells.default = packages.core-python.env;
+      devShells.default = packages.core-python;
 
       formatter = pkgs.alejandra;
 
       packages = flake-utils.lib.flattenTree {
         default = pkgs.core-emu;
-        core-python = pkgs.python3.withPackages (p:
-          with p; [
-            core-emu
-          ]);
-        core-daemon = pkgs.buildFHSUserEnv {
-          name = "core-daemon";
-          targetPkgs = pkgs: (with pkgs; [
-            util-linux
-            coreutils
-            mount
-            umount
-            sysctl
-            nftables
-            ethtool
-            iproute2
-            docker-client
-            packages.core-python
-          ]);
-          runScript = "${packages.core-python}/bin/core-daemon";
-        };
-      };
-
-      apps = {
-        core-cleanup = flake-utils.lib.mkApp {
-          drv = packages.core-python;
-          exePath = "/bin/core-cleanup";
-        };
+        core-python = pkgs.python3.withPackages (p: [p.core-emu]);
       };
     });
 }
--- package.nix
+++ package.nix
@@ -2,7 +2,7 @@
   lib,
   symlinkJoin,
   stdenv,
-  nix-filter,
+  gitignore,
   isPy3k,
   buildPythonPackage,
   pythonRelaxDepsHook,
@@ -51,131 +51,119 @@
   nixosTests,
   pytest,
   mock,
-}:
-buildPythonPackage rec {
-  pname = "core";
-  version = "8.2.0";
-  format = "pyproject";
-  disabled = !isPy3k;
+}: let
+  inherit (gitignore.lib) gitignoreSource;
+in
+  buildPythonPackage rec {
+    pname = "core";
+    version = "8.2.0";
+    format = "pyproject";
+    disabled = !isPy3k;
 
-  src = nix-filter {
-    root = ./.;
-    include = [
-      "daemon"
-      "docs"
-      "gui"
-      "man"
-      "netns"
-      "CHANGELOG.md"
-      "configure.ac"
-      "LICENCE"
-      "Makefile.am"
-      "README.md"
-    ];
-  };
+    src = gitignoreSource ./.;
 
-  preAutoreconf = ''
-    # Create files for AutoReconf
-    touch NEWS README AUTHORS ChangeLog
+    preAutoreconf = ''
+      # Create files for AutoReconf
+      touch NEWS README AUTHORS ChangeLog
 
-    # Patch out path limitation
-    substituteInPlace configure.ac --replace ", \$SEARCHPATH" ""
-  '';
-
-  configureFlags =
-    []
-    ++ lib.optionals withDocs [
-      "--enable-docs"
-    ];
-
-  # Tools are not added to the path if this is true and so configure can't find them
-  strictDeps = false;
-
-  nativeBuildInputs =
-    [
-      autoreconfHook
-      pkg-config
-      pythonRelaxDepsHook
-      makeWrapper
-    ]
-    ++ lib.optionals withDocs [
-      help2man
-      sphinx
-      sphinx-rtd-theme
-    ];
-
-  buildInputs = [
-    nftables
-    procps
-    iproute2
-    ethtool
-    mount
-    umount
-    libev
-    openvswitch
-    imagemagick
-  ];
-
-  pythonRelaxDeps = ["fabric" "grpcio" "invoke" "lxml" "mako" "netaddr" "pillow" "protobuf" "pyproj" "pyyaml"];
-  propagatedBuildInputs = [
-    invoke
-    lxml
-    Mako
-    Fabric
-    poetry
-    pyproj
-    protobuf
-    pillow
-    pyyaml
-    netaddr
-    grpcio
-    grpcio-tools
-    tkinter
-  ];
-
-  buildPhase = ''
-    make
-
-    pushd daemon
-    runHook pipBuildPhase
-    mv dist ../
-    popd
-  '';
-
-  installPhase = ''
-    make install
-    runHook pipInstallPhase
-
-    install -Dm755 daemon/scripts/* -t $out/bin
-
-    wrapProgram $out/bin/core-cleanup \
-      --prefix PATH : ${lib.makeBinPath [procps gnugrep coreutils killall iproute2 gawk nftables]}
-
-    wrapProgram $out/bin/core-daemon \
-      --prefix PATH : ${lib.makeBinPath ([bash nftables iproute2 ethtool libuuid mount procps umount] ++ (lib.lists.optional withDocker docker-client))}
-
-    wrapProgram $out/bin/core-route-monitor --prefix PATH : ${lib.makeBinPath [tcpdump]}
-
-    mkdir -p $out/etc/core
-    cp daemon/data/*.conf $out/etc/core/
-  '';
-
-  checkInputs = [pytest mock];
-  checkPhase = ''
-    pushd daemon
-    pytest -v --mock --lf -x tests
-    popd
-  '';
-
-  meta = with lib; {
-    homepage = "https://coreemu.github.io/core/";
-    description = "CORE (Common Open Research Emulator) is a tool for building virtual networks.";
-    longDescription = ''
-      CORE (Common Open Research Emulator) is a tool for building virtual networks. As an emulator, CORE builds a representation of a real computer network that runs in real time, as opposed to simulation, where abstract models are used. The live-running emulation can be connected to physical networks and routers. It provides an environment for running real applications and protocols, taking advantage of tools provided by the Linux operating system.
+      # Patch out path limitation
+      substituteInPlace configure.ac --replace ", \$SEARCHPATH" ""
     '';
-    license = licenses.bsd2;
-    maintainers = with maintainers; [];
-    platforms = platforms.linux;
-    badPlatforms = platforms.i686;
-  };
-}
+
+    configureFlags =
+      []
+      ++ lib.optionals withDocs [
+        "--enable-docs"
+      ];
+
+    # Tools are not added to the path if this is true and so configure can't find them
+    strictDeps = false;
+
+    nativeBuildInputs =
+      [
+        autoreconfHook
+        pkg-config
+        pythonRelaxDepsHook
+        makeWrapper
+      ]
+      ++ lib.optionals withDocs [
+        help2man
+        sphinx
+        sphinx-rtd-theme
+      ];
+
+    buildInputs = [
+      nftables
+      procps
+      iproute2
+      ethtool
+      mount
+      umount
+      libev
+      openvswitch
+      imagemagick
+    ];
+
+    pythonRelaxDeps = ["fabric" "grpcio" "invoke" "lxml" "mako" "netaddr" "pillow" "protobuf" "pyproj" "pyyaml"];
+    propagatedBuildInputs = [
+      invoke
+      lxml
+      Mako
+      Fabric
+      poetry
+      pyproj
+      protobuf
+      pillow
+      pyyaml
+      netaddr
+      grpcio
+      grpcio-tools
+      tkinter
+    ];
+
+    buildPhase = ''
+      make
+
+      pushd daemon
+      runHook pipBuildPhase
+      mv dist ../
+      popd
+    '';
+
+    installPhase = ''
+      make install
+      runHook pipInstallPhase
+
+      install -Dm755 daemon/scripts/* -t $out/bin
+
+      wrapProgram $out/bin/core-cleanup \
+        --prefix PATH : ${lib.makeBinPath [procps gnugrep coreutils killall iproute2 gawk nftables]}
+
+      wrapProgram $out/bin/core-daemon \
+        --prefix PATH : ${lib.makeBinPath ([bash nftables iproute2 ethtool libuuid mount procps umount] ++ (lib.lists.optional withDocker docker-client))}
+
+      wrapProgram $out/bin/core-route-monitor --prefix PATH : ${lib.makeBinPath [tcpdump]}
+
+      mkdir -p $out/etc/core
+      cp daemon/data/*.conf $out/etc/core/
+    '';
+
+    checkInputs = [pytest mock];
+    checkPhase = ''
+      pushd daemon
+      pytest -v --mock --lf -x tests
+      popd
+    '';
+
+    meta = with lib; {
+      homepage = "https://coreemu.github.io/core/";
+      description = "CORE (Common Open Research Emulator) is a tool for building virtual networks.";
+      longDescription = ''
+        CORE (Common Open Research Emulator) is a tool for building virtual networks. As an emulator, CORE builds a representation of a real computer network that runs in real time, as opposed to simulation, where abstract models are used. The live-running emulation can be connected to physical networks and routers. It provides an environment for running real applications and protocols, taking advantage of tools provided by the Linux operating system.
+      '';
+      license = licenses.bsd2;
+      maintainers = with maintainers; [];
+      platforms = platforms.linux;
+      badPlatforms = platforms.i686;
+    };
+  }
